3f5941b3fcc954689eaa811c05fa555532be04f7
Brendan Lewis <brendanelewis@gmail.com>
2020-09-03 17:45:04 -0400

    Update var_changer.js

--- assets/var_changer.js
+++ assets/var_changer.js
@@ -1,1 +1,14 @@
+// This function taken from "https://www.w3resource.com/javascript-exercises/javascript-date-exercise-18.php"
+function calculate_age(dob) { 
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms); 
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
 
+const AGE = calculate_age(new Date(2003, 5, 14));
+
+let nodes = document.getElementsByClassName("var-changer");
+
+nodes.forEach(function(node) {
+    node.innerHTML = node.innerHTML.replace("AGE_VAR", parseInt(AGE));
+});
