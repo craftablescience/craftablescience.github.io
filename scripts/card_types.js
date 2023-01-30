b3a7f0a008bd07ba516b40ef61f2429db7b1aea6
craftablescience <me@craftablescience.info>
2023-01-29 22:20:16 -0500

    feat: add about section

--- scripts/card_types.js
+++ scripts/card_types.js
@@ -6,21 +6,28 @@ customElements.define("project-card", class extends HTMLElement {
 			disclaimer = "<em>I am a member of a team that works on this project.</em> ";
 		}
 
+		let sourceUrl = "";
+		if (this.hasAttribute("src-link")) {
+			sourceUrl = `<p class="align-center"><a href="${this.getAttribute("src-link")}" target="_blank" rel="noopener noreferrer">View Source</a></p>`;
+		}
+
 		let projectUrl = "";
 		if (this.hasAttribute("mod-link")) {
 			projectUrl = `<p class="align-center"><a href="${this.getAttribute("mod-link")}" target="_blank" rel="noopener noreferrer">Download from CurseForge</a></p>`;
 		} else if (this.hasAttribute("play-link")) {
 			projectUrl = `<p class="align-center"><a href="${this.getAttribute("play-link")}" target="_blank" rel="noopener noreferrer">Play Game</a></p>`;
+		} else if (this.hasAttribute("steam-link")) {
+			projectUrl = `<p class="align-center"><a href="${this.getAttribute("steam-link")}" target="_blank" rel="noopener noreferrer">View Steam Store Page</a></p>`;
 		}
 
 		this.innerHTML = `
-			<img src="${this.getAttribute("img-src")}" alt="${this.getAttribute("img-alt")}" title="${this.getAttribute("img-alt")}" />
+			<img src="${this.getAttribute("img-src")}" alt="${this.getAttribute("img-alt")}" title="${this.getAttribute("img-alt")}" class="shadow-border" />
 			<div>
 				<h2 class="align-center">${this.getAttribute("heading")}</h2>
 				<h4 class="card-subheading align-center">${this.getAttribute("subheading")}</h4>
 				<p class="align-justify">${disclaimer}${this.getAttribute("description")}</p>
 				<br />
-				<p class="align-center"><a href="${this.getAttribute("src-link")}" target="_blank" rel="noopener noreferrer">View Source</a></p>
+				${sourceUrl}
 				${projectUrl}
 			</div>
 		`;
