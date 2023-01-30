734dcd451c57703fc2e129ba5028938670f4c7c3
craftablescience <me@craftablescience.info>
2023-01-30 00:22:37 -0500

    holy fucking bingle what?! :3

---
+++ scripts/bingle.js
@@ -0,0 +1,13 @@
+console.log(`holy fucking bingle! :3
+thanks for looking at this
+u are a very cool creature
+
+i don't really know what to say sooo
+trans rights are human rights
+garfield caused 9/11
+capitalism? more like CRAPitalism
+ok anyway hope u like the site
+and also if youre a recruiter please hire me
+(unless youre in the military business)
+ok byeeeeeeeee
+`);
