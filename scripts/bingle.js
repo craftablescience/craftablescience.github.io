9b7089f3611fbc3538c91a5a73f74e231181fe4d
craftablescience <me@craftablescience.info>
2023-02-26 15:44:52 -0500

    feat: add backup bot project

--- scripts/bingle.js
+++ scripts/bingle.js
@@ -3,11 +3,8 @@ thanks for looking at this
 u are a very cool creature
 
 i don't really know what to say sooo
-trans rights are human rights
-garfield caused 9/11
-capitalism? more like CRAPitalism
-ok anyway hope u like the site
+trans rights are human rights and garfield caused 9/11
+ok hope u like the site
 and also if youre a recruiter please hire me
 (unless youre in the military business)
-ok byeeeeeeeee
-`);
+ok byeeeeeeeee`);
