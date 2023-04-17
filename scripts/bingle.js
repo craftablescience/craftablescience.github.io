a3304e3b767cc126d145e560433a89a9c7f1cd80
craftablescience <me@craftablescience.info>
2023-04-17 03:32:25 -0400

    feat: add checking out project

--- scripts/bingle.js
+++ scripts/bingle.js
@@ -1,10 +1,8 @@
 console.log(`holy fucking bingle! :3
 thanks for looking at this
-u are a very cool creature
+you are a very cool creature
 
-i don't really know what to say sooo
-trans rights are human rights and garfield caused 9/11
-ok hope u like the site
+i don't really know what to say sooo, hope you like the site
 and also if youre a recruiter please hire me
 (unless youre in the military business)
 ok byeeeeeeeee`);
