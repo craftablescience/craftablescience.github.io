efae4cf39a089f13fe59c2c7fb279f7ac0d53bd6
craftablescience <me@craftablescience.info>
2023-08-07 03:44:24 -0400

    fix: tone down bingle.js

--- scripts/bingle.js
+++ scripts/bingle.js
@@ -1,8 +1,5 @@
-console.log(`holy fucking bingle! :3
-thanks for looking at this
-you are a very cool creature
-
-i don't really know what to say sooo, hope you like the site
+console.log(`i don't really know what to say here
+hope you like the site
 and also if youre a recruiter please hire me
 (unless youre in the military business)
-ok byeeeeeeeee`);
+ok bye!`);
